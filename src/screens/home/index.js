771658091ac00ad513c0b8c9179e37c89ad5d1b6
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-15 06:35:38 +0800

    Refactor code to match Ryan Florence's Folder Structure https://gist.github.com/ryanflorence/daafb1e3cb8ad740b346

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-class App extends Component {
+import './index.css'
+import logo from './logo.svg'
+
+class Home extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img alt="logo" className="App-logo" src={logo} />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
             href="https://reactjs.org"
-            target="_blank"
             rel="noopener noreferrer"
+            target="_blank"
           >
             Learn React
           </a>
@@ -25,4 +26,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default Home
