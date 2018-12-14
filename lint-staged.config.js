6aed41336ca9a193ddb51d5de364f607ff23fb53
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-14 12:25:36 +0800

    Setup Cypress CI

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '**/*.+(js|json|less|css|ts|tsx|md)': [
       'prettier',
       'yarn test --coverage --findRelatedTests',
+      'yarn build -s',
       'git add',
     ],
   },
