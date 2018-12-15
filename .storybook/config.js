c2abafec70ba56ea3daf49ed1f321cd630c9da91
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-15 12:15:31 +0800

    Add Home stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,12 @@
 import { configure } from '@storybook/react'
 
+import '../src/global-styles.css'
+
+// Load all .stories.js files under __stories__ folder
+const req = require.context('../src', true, /\/__stories__\/.+\.stories\.js$/)
+
 function loadStories() {
-  require('../src/__stories__')
+  req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module)
---
+++ src/screens/home/__stories__/index.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Home from '..'
+
+storiesOf('home', module).add('index', () => <Home />)
