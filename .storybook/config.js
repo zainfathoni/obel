79a19a0fa5762692a3fd3076eab8286f185b840b
Zain Fathoni <zain.fathoni@gmail.com>
2019-08-17 05:50:28 +0800

    Upgrade storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react'
 
-import '../src/global-styles.css'
+import '../src/index.css'
 
 // Load all .stories.js files under __stories__ folder
 const req = require.context('../src', true, /\/__stories__\/.+\.stories\.js$/)
