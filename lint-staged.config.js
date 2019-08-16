8819c21f7cc7cf028967d162c657a69c4575d97e
Zain Fathoni <zain.fathoni@gmail.com>
2019-08-17 06:18:44 +0800

    Upgrade husky and lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  linters: {
-    '**/*.+(js|json|less|css|ts|tsx|md)': [
-      'prettier',
-      'yarn test --coverage --findRelatedTests',
-      'yarn build:react -s',
-      'git add',
-    ],
-  },
+  '**/*.+(js|json|less|css|ts|tsx|md)': [
+    'prettier',
+    'yarn test --coverage --findRelatedTests',
+    'yarn build:react -s',
+    'git add',
+  ],
 }
