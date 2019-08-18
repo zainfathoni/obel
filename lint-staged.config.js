aaeee5743a9eb386dde9743f9187f271807009ed
Zain Fathoni <zain.fathoni@gmail.com>
2019-08-18 23:48:11 +0800

    Separate CSS & Markdown pre-commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  'src/**/*.{js,json,css,md}': [
+  'src/**/*.{js,json}': [
     'prettier-eslint --write',
     'npx sortier',
     'eslint',
     'git add',
     'yarn test:staged',
     'yarn build:react -s'
+  ],
+  'src/**/*.{css,md}': [
+    'prettier-eslint --write',
+    'npx sortier',
+    'git add'
   ]
 }
