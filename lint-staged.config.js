4500a18b83afba8512ff0af7803bcbf408370ccb
Zain Fathoni <zain.fathoni@gmail.com>
2019-08-18 13:36:13 +0800

    Edit lint-staged config to not watch tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   '**/*.+(js|json|less|css|ts|tsx|md)': [
     'prettier',
-    'yarn test --findRelatedTests',
+    'yarn test:staged',
     'yarn build:react -s',
     'git add',
   ],
