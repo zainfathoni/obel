11713f1d867e84935fe413dd1315c56cf92ea2a0
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2019-08-21 11:10:02 +0700

    add simple test for a11y

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,9 +1,26 @@
-import { render } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
+import { axe, toHaveNoViolations } from 'jest-axe'
 import React from 'react'
 import App from '../App'
+
+expect.extend(toHaveNoViolations)
 
 it('renders without crashing', () => {
   const { getByText } = render(<App />)
   const ojekNav = getByText(/^ojek$/i)
   expect(ojekNav).toBeDefined()
+})
+
+it('check for a11y violations', async () => {
+  const { container } = render(<App/>)
+  const results = await axe(container)
+  expect(results).toHaveNoViolations()
+  cleanup()
+})
+
+it('should be breaking a11y violations', async () => {
+  const Img = () => <img src="#"/>
+  const { container } = render(<Img/>)
+  const results = await axe(container)
+  expect(results).toHaveNoViolations()
 })
