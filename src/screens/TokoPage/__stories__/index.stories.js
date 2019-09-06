d9cf35b081ba67d3268fe4d608a378530495c5de
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2019-09-06 23:42:01 +0700

    Add new Screen Toko Page

---
+++ src/screens/TokoPage/__stories__/index.stories.js
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react'
+import React from 'react'
+import TokoPage from '../index'
+
+storiesOf('screens/TokoPage', module).add('index', () => <TokoPage />)
---
+++ src/screens/TokoPage/item.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    description: 'Dicincang halus dengan mesin',
+    harga: 30000,
+    item_name: 'Daging Ayam Cincang'
+  },
+  { description: 'Dengan kepala', harga: 45000, item_name: 'Ayam Utuh' },
+  { description: 'Masih Segar', harga: 22000, item_name: 'Ikan Kakap' },
+  { description: 'Kualitas Asli', harga: 39000, item_name: 'Ikan Bandeng' },
+  { description: 'Berkualitas', harga: 24000, item_name: 'Jamur Tiram' },
+  {
+    description:
+      'Berkualitas bersumber dari pengolahan jamur alami terpercaya dan bersertifikat halal',
+    harga: 24000,
+    item_name: 'Jamur Kuping'
+  },
+  {
+    description: 'Alami dan berkualitas',
+    harga: 4000,
+    item_name: 'Tahu Kuning'
+  },
+  {
+    description: 'Asli dan berkualitas',
+    harga: 10000,
+    item_name: 'Tape Ketan'
+  }
+]
