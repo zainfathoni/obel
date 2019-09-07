02b6375e5038bc0b799f56c0094245be5383dd9e
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2019-09-07 13:24:59 +0700

    improve responsive card

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,26 @@ import sample from './../screens/TokoPage/sample.jpg'
 
 function Card ({ item }) {
   return (
-    <div className='lg:w-1/3 md:w-1/3 sm:w-1/2 pb-3 pl-1 pr-1'>
+    <div className='lg:w-1/3 md:w-1/3 sm:w-1/2 w-1/2 pb-3 pl-1 pr-1'>
       <div className='rounded overflow-hidden bg-gray-200 shadow-lg border-solid border-2 border-gray-600'>
-        <img alt='Sunset in the mountains' className='w-full' src={sample} />
-        <div className='px-6 py-4'>
-          <div className='font-bold text-xl mb-2'>{item.item_name}</div>
-          <p className='text-gray-700 text-base'>{item.description}</p>
-          <p className='text-sm font-bold'>Harga : {item.harga} / Kg</p>
+        <img
+          alt='Sunset in the mountains'
+          className='w-full h-32 sm:h-56 object-cover'
+          src={sample}
+        />
+        <div className='sm:px-6 sm:py-4 px-3 py-3'>
+          <div className='font-bold sm:text-xl text-sm mb-2'>
+            {item.item_name}
+          </div>
+          <p className='text-gray-700 text-xs sm:text-base'>
+            {item.description}
+          </p>
+          <p className='text-xs sm:text-base font-bold'>
+            Harga : {item.harga} / Kg
+          </p>
           <button className='w-full my-3 bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
             <FontAwesomeIcon icon={faShoppingCart} />
-            <span className='ml-2'>Beli</span>
+            <span className='ml-2 text-xs sm:text-base'>Beli</span>
           </button>
         </div>
       </div>
