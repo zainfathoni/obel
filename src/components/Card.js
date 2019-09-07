e82c6b5dbec6592953c9e2a93831820858ed39a7
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2019-09-07 14:52:36 +0700

    Add custom tailwind utilities to reset box sizing (cause scrambled display on netlify build)

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ import sample from './../screens/TokoPage/sample.jpg'
 
 function Card ({ item }) {
   return (
-    <div className='lg:w-1/3 md:w-1/3 sm:w-1/2 w-1/2 pb-3 pl-1 pr-1'>
+    <div className='lg:w-1/3 md:w-1/3 sm:w-1/2 w-1/2 pb-3 pl-1 pr-1 border-box'>
       <div className='rounded overflow-hidden bg-gray-200 shadow-lg border-solid border-2 border-gray-600'>
         <img
           alt='Sunset in the mountains'
@@ -23,7 +23,7 @@ function Card ({ item }) {
           <p className='text-xs sm:text-base font-bold'>
             Harga : {item.harga} / Kg
           </p>
-          <button className='w-full my-3 bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+          <button className='w-full my-3 bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 rounded'>
             <FontAwesomeIcon icon={faShoppingCart} />
             <span className='ml-2 text-xs sm:text-base'>Beli</span>
           </button>
